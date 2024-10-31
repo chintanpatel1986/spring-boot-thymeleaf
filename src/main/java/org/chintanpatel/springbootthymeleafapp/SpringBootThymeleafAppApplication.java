6e8d2c51b37a46a5db766304664fd0a120829cae
chintanpatel <chintanpatel2013@icloud.com>
2024-10-31 15:25:27 -0400

    create spring boot with thymeleaf template web application.

---
+++ src/main/java/org/chintanpatel/springbootthymeleafapp/SpringBootThymeleafAppApplication.java
@@ -0,0 +1,13 @@
+package org.chintanpatel.springbootthymeleafapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThymeleafAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chintanpatel/springbootthymeleafapp/SpringBootThymeleafAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chintanpatel.springbootthymeleafapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
